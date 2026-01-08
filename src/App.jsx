3d32658eaf8e6f88956dc4d5f9d48db29bd1f0bb
Ishika <operation21@zillion.io>
2026-01-08 22:28:28 +0530

    Controller Component/forms/input/state/button/events

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-
-import { useState } from "react";
-
-
+import { useState }  from "react";
 function App() {
-  const [val,setVal]=useState("Anil sindhu")
-  return (
-    <div>
-      <h1>Input field</h1>
-
-      {/* <input type="text" onChange={(event)=>alert(event.target.value)} placeholder="Enter your name" /> */}
-       <input type="text" value = {val} onChange={(event)=>setVal(event.target.value)} placeholder="Enter your name" />
-      <br />
-
-    <h1>{val}</h1>
-    <button onClick={()=>setVal("")}>Clear Value</button>
-    </div>
-  );
+  const [name , setName]= useState("");
+  const [password , setPassword] = useState("");
+  const [email , setEmail] = useState("");
+  return(
+  <div>
+    <h1>Controller Component</h1>
+    {/* <form action="" method=""> */}
+      <input type="text" onChange={(event)=>setName(event.target.value)} placeholder="Enter Name" />
+      <br/>
+      <input type="password" onChange={(event)=>setPassword(event.target.value)} placeholder="Enter password" />
+      <br/>
+      <input type="text" onChange={(event)=>setEmail(event.target.value)} placeholder="Enter email" />
+      <br/>
+      <button>Submit</button>
+      <button onClick={()=>{setName(""); setPassword(""); setEmail("");}}>Clear</button>
+      <h1>{name}</h1>
+      <h1>{password}</h1>
+      <h1>{email}</h1>
+    {/* </form> */}
+  </div>
+)
+    
+ 
 }
 
 export default App;
