a95acec4a0122eb4a202b3233ee8a6070840975b
Ishika <operation21@zillion.io>
2026-01-15 12:12:50 +0530

    in this i have learned Styled Component in the react it is external package i have install with the styledcomponnet npm sa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import './css/style.css'
+import styled from "styled-components";
 function App() {
+  const Heading=styled.h1`
+  color:red;
+  border:1px solid green;
+  border radius:20px;
+  padding:20px
+  `
+
+  const StyleBtn=styled.button`
+  color:red;
+  width:130px
+  height:40px;
+  margin:20px
+  `
   return(
     <div>
-      <h1 className='heading'>External Style</h1>
+     
+      <Heading>Hello Heading</Heading>
+      <Heading>Hello Heading</Heading>
+      <Heading>Hello Heading</Heading>
+      <Heading>Hello Heading</Heading>
+     <StyleBtn>Login</StyleBtn>
+      <StyleBtn>Signup</StyleBtn>
+       <StyleBtn>Logout</StyleBtn>
+        <StyleBtn>Delete</StyleBtn>
     </div>
   );
 
 }
 export default App;
+
+// benifit not just give every h1 have diff style just use one defined pre
