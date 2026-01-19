bd0ad46d5f06e2810cba7cb29e46558bb42d1835
Ishika <operation21@zillion.io>
2026-01-19 14:56:40 +0530

    In which i have made one new nav bar in which when i click on home routre chnage and in about route change and in login as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import { BrowserRouter , Routes , Route ,Link } from "react-router";
-import Home from "./home"
-import About from "./About"
-import Contact from "./Contact"
+import NavBar from "./NavBar";
+
 
 function App() {
   
-  return (
-    <div>
-      <BrowserRouter>
-      <Link to="/"><h1>Home</h1></Link>
-      <Link to="/about"><h1>About</h1></Link>
-      <Link to="/contact"><h1>Conatct</h1></Link>    
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-         <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-        
-      </Routes>
-      </BrowserRouter>
-    </div>
-  );
+return(
+ <div>
+    <BrowserRouter>
+      <NavBar/>
+    </BrowserRouter>
+
+ </div>
+);
 }
 
 export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,30 @@
+import {Link} from "react-router"
+import'./header.css'
+function NavBar(){
+    return(
+        <div className="header">
+        <div>
+            <Link className="link"><h1>Logo</h1></Link>
+        </div>
+<div>
+    <ul>
+        <li>
+            <Link className="link" to="/"><h2>Home</h2></Link>
+        </li>
+        <li>
+            <Link className="link" to="/logIn"><h2>Contact</h2></Link>
+        </li>
+        <li>
+            <Link className="link" to="/About"><h2>About</h2></Link>
+        </li>
+    </ul>
+</div>
+
+        </div>
+
+    );
+}
+export default NavBar;
+
+
+
