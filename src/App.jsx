6b7a9741f009cbf6bb5cb83136f767e2144ad7b9
Ishika <operation21@zillion.io>
2026-01-19 11:21:15 +0530

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import Functionasprops from "./Functionasprops";
+import { BrowserRouter , Routes , Route ,Link } from "react-router";
 
 function App() {
-  const displayName = () => {
-    alert("hi");
-  };
-
+  
   return (
     <div>
-      <h1>Function as props</h1>
-      <Functionasprops displayName={displayName} />
+      <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to ="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}/>
+         <Route path="/about" element={<h1>About</h1>}/>
+          <Route path="/contact" element={<h1>Contact</h1>}/>
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
