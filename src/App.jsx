58e0c5bd709f637051194e7b66f8829142b01434
Ishika <operation21@zillion.io>
2026-01-19 12:02:31 +0530

    Routes Page through react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter , Routes , Route ,Link } from "react-router";
+import Home from "./home"
+import About from "./About"
+import Contact from "./Contact"
 
 function App() {
   
   return (
     <div>
       <BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to ="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/"><h1>Home</h1></Link>
+      <Link to="/about"><h1>About</h1></Link>
+      <Link to="/contact"><h1>Conatct</h1></Link>    
       <Routes>
-        <Route path="/" element={<h1>Home</h1>}/>
-         <Route path="/about" element={<h1>About</h1>}/>
-          <Route path="/contact" element={<h1>Contact</h1>}/>
+        <Route path="/" element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
         
       </Routes>
       </BrowserRouter>
