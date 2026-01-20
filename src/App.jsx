b32aca1fa9c693c366a661f9256ccca805e9e8ca
Ishika <operation21@zillion.io>
2026-01-20 16:35:20 +0530

    IN which i have learn Get api Data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { BrowserRouter , Routes , Route ,Link } from "react-router";
-import NavBar from "./NavBar";
-
-
+import { useEffect,useState } from "react";
+import './Api.css';
 function App() {
-  
+  const [usersData,setUserData]=useState([]);
+
+  useEffect (()=>{
+    getUserdata();
+
+  },[])
+ 
+ 
+  async function getUserdata(){
+    const url ="https://dummyjson.com/user";
+    let response =await fetch (url);
+    response = await response.json()
+    
+    setUserData(response.users);
+
+
+  }
+  console.log(usersData);
 return(
  <div>
-    <BrowserRouter>
-      <NavBar/>
-    </BrowserRouter>
+    
+<h1>Fetch Data from Get Api</h1>
+{
+  usersData && usersData.map((user)=>{
+    return(
+   <ul className="user-list">
+   
+   <li> {user.firstName}</li>
+    <li>{user.lastName}</li>
+    <li>{user.age}</li>
+    <li>{user.email}</li>
 
+   </ul>
+    )
+
+  })
+}
  </div>
 );
 }
---
+++ src/Error404.jsx
@@ -0,0 +1,15 @@
+function Error() {
+  return (
+    <div>
+      <h1>404 Error</h1>
+      <h1>Page Not Found</h1>
+      <img
+        src="https://syhzhuelbxgnhopnwjgc.supabase.co/storage/v1/object/public/media/blog/404_page_cover.jpg"
+        alt="404 Page Not Found"
+        width="400"
+      />
+    </div>
+  );
+}
+
+export default Error;
